cb203158887ed8c6ba7c393268a5f1b74529cf0e
Najrul Ahmed <33804460+Najrul787@users.noreply.github.com>
2017-12-01 14:54:12 +0600

    Reduce code
    1. add new css function for animation
    2. remove span & space function 
    3. add letter function

--- demo/text-animation.js
+++ demo/text-animation.js
@@ -1,12 +1,11 @@
 /**
  * Author 		: Najrul Ahmed
- * Author URL 	: http://www.facebook.com/najrul787
+ * facebook 	: http://www.facebook.com/najrul787
  * From 		: Bangladesh
  * plugin Name 	: Quick Text Animation
  * Created 		: March 15, 2016
  * Update 		: DESC 1, 2017
- * Contact		: +8801838188585
  */
-(function(g){g.fn.animQText=function(b){var a=g(this);b=b||{};a.interval=!1;a.length=0;a.count=0;a.revers=!1;var h=b.effectIn||"rollIn",k=b.effectOut||"rollOut",d=b.delay||800,f=b.duration||500,l=!1===b.revers?!1:!0;a.span=function(c){a.length+=1;return'<span class="qtt" style="display:inline-block;animation-duration:'+f+'ms">'+c+"</span>"};a.space=function(a){return'<i style="display:inline-block;" class="qts">&nbsp;</i>'};a.empty=function(){a.html("")};a.resetEffect=function(){a.items.removeClass(h);
-a.items.removeClass(k)};a.addEffect=function(){a.revers?l?(a.count--,a.items.eq(a.count).addClass(k),0>=a.count&&(a.revers=!1)):(a.revers=!1,a.count=0):(a.items.eq(a.count).addClass(h),a.count>a.length&&(a.revers=!0),a.count++)};a.processText=function(){for(var c="",b=a.text().split(""),d=b.length,e=0;e<d;e++)c=b[e].trim()?c+a.span(b[e]):c+a.space();return c};a.setText=function(){var b=a.processText();a.empty();a.html(b)};a.start=function(){a.setText();a.items=a.children("span.qtt");f>d&&(d=f);setInterval(a.anim,
-d)};a.anim=function(){a.resetEffect();a.addEffect()};a.start()}})(jQuery);
+(function(c){c.fn.animQText=function(b){var a=c(this);b=b||{};a.interval=!1;a.length=0;a.count=0;a.revers=!1;a.letters="";var h=b.effectIn||"rollIn",k=b.effectOut||"rollOut",g=b.delay||800,f=b.duration||500,l=!1===b.revers?!1:!0;a.letter=function(e){e.trim()?(a.letters+='<span class="qtt">'+e+"</span>",a.length+=1):a.letters+='<i class="qts">&nbsp;</i>'};a.resetEffect=function(){a.items.removeClass(h);a.items.removeClass(k)};a.css=function(){var e=a.selector,b=document.createElement("style"),d=e+
+" qtt{display:inline-block;-webkit-animation-duration: "+f+"ms;animation-duration: "+f+"ms;-webkit-animation-fill-mode: both;animation-fill-mode: both;}";d+=e+" qts{display:inline-block;}";c(b).html(d);c("body").append(b)};a.addEffect=function(){a.revers?l?(a.count--,a.items.eq(a.count).addClass(k),0>=a.count&&(a.revers=!1)):(a.revers=!1,a.count=0):(a.items.eq(a.count).addClass(h),a.count>a.length&&(a.revers=!0),a.count++)};a.processText=function(){for(var b=a.text().split(""),c=b.length,d=0;d<c;d++)a.letter(b[d]);
+a.html(a.letters)};a.start=function(){a.processText();a.css();a.items=a.children("span.qtt");f>g&&(g=f);setInterval(a.anim,g)};a.anim=function(){a.resetEffect();a.addEffect()};a.start()}})(jQuery);
